ac17c7d112382a415ef2025ece44a7a7e905d125
ENDISit <mr.ilia08@mail.ru>
2024-06-07 00:13:16 +0300

    Добавьте файлы проекта.

---
+++ Countdown/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+class Countdown
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество секунд для обратного отсчета:");
+        int seconds = int.Parse(Console.ReadLine());
+
+        for (int i = seconds; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(1000); // Ожидание 1 секунду
+        }
+        Console.WriteLine("Обратный отсчет завершен!");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
+    }
+}
